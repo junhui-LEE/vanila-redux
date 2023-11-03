9e883674a30ad4a8451b59fd3cbb56d698d9f57f
junhui-LEE <junhee0386@naver.com>
2023-11-03 10:24:25 +0900

    1.2 Actions - 초보자를 위한 리덕스 101 (노마드 코더)

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
-// git 계정 테스트 2
+import { createStore } from "redux";
+
+const add = document.getElementById("add");
+const minus = document.getElementById("minus");
+const number = document.querySelector("span");
+
+const countModifier = (count = 0, action) => {
+    console.log(count, action);
+    if(action.type === "ADD"){
+        return count + 1;
+    }else if(action.type === "MINUS"){
+        return count - 1;
+    }else{
+        return count;
+    }
+}
+
+const countStore = createStore(countModifier);
+
+countStore.dispatch({type : "ADD"});
+countStore.dispatch({type : "ADD"});
+countStore.dispatch({type : "ADD"});
+countStore.dispatch({type : "ADD"});
+countStore.dispatch({type : "ADD"});
+countStore.dispatch({type : "MINUS"});
+
+console.log(countStore.getState());
